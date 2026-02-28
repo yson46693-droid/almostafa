6e6198b748bcd7b12686c9de358e07a8fa12c085
osama <osama@osamas-Air.home>
2026-02-28 14:52:41 +0200

    Update - 2026-02-28 14:52

--- dashboard/manager.php
+++ dashboard/manager.php
@@ -423,6 +423,22 @@ if ($page === 'company_cash' && isset($_GET['ajax'])) {
     }
 }
 
+// معالجة طلبات AJAX لصفحة طلبات شركات الشحن (كشف الحساب والخصم) قبل أي إخراج لضمان استجابة JSON فقط
+if ($page === 'shipping_orders' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $isAjaxShipping = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+    $actionShipping = $_POST['action'] ?? '';
+    if ($isAjaxShipping && in_array($actionShipping, ['get_shipping_company_statement', 'deduct_from_shipping_company'], true)) {
+        while (ob_get_level() > 0) {
+            ob_end_clean();
+        }
+        $modulePath = __DIR__ . '/../modules/manager/shipping_orders.php';
+        if (file_exists($modulePath)) {
+            include $modulePath;
+            exit;
+        }
+    }
+}
+
 // معالجة AJAX لجلب أرقام الهواتف من صفحة local_customers
 if ($page === 'local_customers' && 
     $_SERVER['REQUEST_METHOD'] === 'GET' && 
